13c9f1ba61b771c56e68a6f9ab2e7751aede4756
VladislavKondakov <agentvl@ukr.net>
2023-03-07 01:20:11 +0100

    д

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,48 +1,47 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import {galleryItems} from './gallery-items.js';
 
-// console.log(galleryItems);
+const GalleryDiv = document.querySelector('.gallery');
 
-const galleryDiv = document.querySelector('.gallery');
-galleryDiv.addEventListener('click', galleryDivClick);
+GalleryDiv.addEventListener('click', GalleryDivClick);
 
 const cardsGallery = createGalleryCards(galleryItems);
-galleryDiv.insertAdjacentHTML('beforeend', cardsGallery);
+GalleryDiv.insertAdjacentHTML('beforeend', cardsGallery);
 
 function createGalleryCards(items) {
-    return items.map(({ preview, original, description }) => {
-        return ` 
-    <div class="gallery__item">
+  return items.map(({preview, original, description}) => {
+    return `
+      <div class="gallery__item">
         <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
         </a>
-    </div>
+      </div>
     `;
-    }).join('');
-};
+  }).join('');
+}
 
-function galleryDivClick(event) {
-    event.preventDefault();
-    if (!event.target.classList.contains('gallery__image')) {
-        return;
-    }
+function GalleryDivClick(event) {
+  event.preventDefault();
 
-    modalWindow(event.target.dataset.source);
-};
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  modalWindow(event.target.dataset.source);
+}
 
 function modalWindow(url) {
-    const modal = basicLightbox.create(`
-		<img width="800" height="600" src="${url}">
-	`);
+  const modal = basicLightbox.create(`
+    <img width="800" height="600" src="${url}">
+  `);
 
-    modal.show();
-   
-    document.addEventListener('keydown', handleEscKeydown);
+  modal.show();
 
-    function handleEscKeydown(event) {
-        if (event.code === 'Escape') {
-            modal.close();
-        document.removeEventListener('keydown', handleEscKeydown);
-        }
+  document.addEventListener('keydown', handleEscKeydown);
+
+  function handleEscKeydown(event) {
+    if (event.code === 'Escape') {
+      modal.close();
+      document.removeEventListener('keydown', handleEscKeydown);
     }
-};
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,20 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+console.log(galleryItems);
 
-// console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
 
-
-const galleryDiv = document.querySelector('.gallery');
-
-const cardsGallery = createGalleryCards(galleryItems);
-galleryDiv.insertAdjacentHTML('beforeend', cardsGallery);
-
-function createGalleryCards(items) {
-    return items.map(({ preview, original, description }) => {
-        return ` 
+const createGalleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
     <a class="gallery__item" href="${original}">
-         <img class="gallery__image" src="${preview}" alt="${description}" />
-    </a>
-    `;
-    }).join('');
-    
-};
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+  </a>`;
+  })
+  .join("");
 
-const lightbox = new SimpleLightbox('.gallery a',
-    {
-        captionsData: 'alt',
-        captionDelay: 250,
-        scrollZoom: false,
-    }
-);
+galleryContainer.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
