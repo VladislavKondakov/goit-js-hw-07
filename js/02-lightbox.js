56f998e18b36bc0b10e2081bb6259f6cd96c16f6
VladislavKondakov <agentvl@ukr.net>
2023-03-07 01:28:27 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,31 +1,45 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
-
-const galleryEl = document.querySelector(`.gallery`)
+const galleryEl = document.querySelector('.gallery');
 const imgMarkup = createGallery(galleryItems);
-galleryEl.insertAdjacentHTML('beforeend', imgMarkup)
+galleryEl.insertAdjacentHTML('beforeend', imgMarkup);
 
 function createGallery(gallery) {
-    return gallery.map(({ preview, original, description }) => {
-        return `
-            <li>
-                <a class="gallery__link" href="${original}">
-                    <img
-                    class="gallery__image"
-                    src="${preview}"
-                    data-source="${original}"
-                    alt="${description}"
-                    />
-                </a>
-            </li>
-        `;
-    })
-    .join('')
+  return gallery.map(({ preview, original, description }) => {
+    return `
+      <li>
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  }).join('');
 }
 
-const galleryLightBox = new SimpleLightbox(`.gallery a`, {
-    captionsData: `alt`,
-    captionDelay: `250`,
+galleryEl.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  const { target } = event;
+
+  if (target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const originalImageURL = target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${originalImageURL}" width="800" height="600">`
+  );
+
+  instance.show();
+}
+
+const galleryLightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
 });
